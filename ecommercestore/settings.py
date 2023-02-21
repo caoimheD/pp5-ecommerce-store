9d5bc330ab0f814281857a6d88ade12f7832ac18
caoimheD <caoimhe.db5@gmail.com>
2023-02-21 18:53:26 +0000

    create home app and index.html

--- ecommercestore/settings.py
+++ ecommercestore/settings.py
@@ -43,7 +43,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'cloudinary_storage',
     'django.contrib.staticfiles',
-    'cloudinary'
+    'cloudinary',
+    'home',
 ]
 
 LOGIN_REDIRECT_URL = '/'
