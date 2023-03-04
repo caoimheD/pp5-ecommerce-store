25c56283d677858faada4c5aff41e5b179f0a31b
caoimheD <caoimhe.db5@gmail.com>
2023-03-04 12:26:51 +0000

    create product views, urls and templates

--- products/urls.py
+++ products/urls.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from django.urls import path
 from . import views
+from products.views import ProductList, ProductDetail
 
 urlpatterns = [
+    path('', ProductList.as_view(), name='products'),
+    path('<int:pk>/', ProductDetail.as_view(), name='productdetail'),
 ]
--- products/views.py
+++ products/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from .models import Product, Category
+from django.views.generic import ListView, DetailView, UpdateView, \
+    CreateView, DeleteView
 
 # Create your views here.
+
+
+class ProductList(ListView):
+    model = Product
+    template_name = '../templates/products/products.html'
+    context_object_name = 'products'
+
+
+class ProductDetail(DetailView):
+    model = Product
+    template_name = '../templates/products/product_detail.html'
+    context_object_name = 'productdetails'
