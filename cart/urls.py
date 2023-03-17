fa48ad0ee14234975abc1d410b74fcdc398640b7
caoimheD <caoimhe.db5@gmail.com>
2023-03-17 17:17:54 +0000

    create view and url for deleting items from cart

--- cart/urls.py
+++ cart/urls.py
@@ -1,11 +1,13 @@
 from django.contrib import admin
 from django.urls import path
 from . import views
-from .views import CreateCart, add_to_cart
+from .views import CreateCart, add_to_cart, edit_cart, remove_from_cart
 
 urlpatterns = [
     path('', views.cart, name='cart'),
     path('addcart/', CreateCart.as_view(),
          name='addcart'),
     path('add/<item_id>/', views.add_to_cart, name='add_to_cart'),
+    path('edit/<item_id>/', views.edit_cart, name='edit_cart'),
+    path('remove/<item_id>/', views.remove_from_cart, name='remove_from_cart'),
 ]
--- cart/views.py
+++ cart/views.py
@@ -8,6 +8,7 @@ from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
+
 @login_required
 def cart(request):
     """ Displays cart page """
@@ -43,7 +44,7 @@ class CreateCart(LoginRequiredMixin, CreateView):
 
 @login_required
 def add_to_cart(request, item_id):
-    """ Add a quantity of the specified product to the shopping bag """
+    """ Add a quantity of the specified product to the cart """
 
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
@@ -56,4 +57,30 @@ def add_to_cart(request, item_id):
 
     request.session['cart'] = cart
     return redirect(redirect_url)
+
+
+def edit_cart(request, item_id):
+    """Adjust the quantity of the specified product to the specified amount"""
+
+    quantity = int(request.POST.get('quantity'))
+    cart = request.session.get('bag', {})
+
+    if quantity > 0:
+        cart[item_id] = quantity
+    else:
+        cart.pop(item_id)
+
+    request.session['cart'] = cart
+    return redirect(reverse('cart'))
+
+
+def remove_from_cart(request, item_id):
+    cart = request.session.get('cart', {})
+    cartitems = list(cart.keys())
+
+    for item_id in list(cart.keys()):
+        cart.pop(item_id, None)
     
+    print(cartitems)
+    request.session['cart'] = cart
+    return redirect('cart')
