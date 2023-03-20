6b33d74ce146c227548ccc8e42c151638f0913c8
caoimheD <caoimhe.db5@gmail.com>
2023-03-20 10:05:02 +0000

    add success messages to settings, product details and cart

--- cart/views.py
+++ cart/views.py
@@ -4,6 +4,7 @@ from django.views.generic import ListView, DetailView, UpdateView, \
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse, reverse_lazy
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
 # Create your views here.
 
@@ -19,14 +20,16 @@ def cart(request):
 def add_to_cart(request, item_id):
     """ Add a quantity of the specified product to the cart """
 
-    quantity = int(request.POST.get('quantity'))
+    quantity = int(request.POST.get('quantity', 1))
     redirect_url = request.POST.get('redirect_url')
     cart = request.session.get('cart', {})
 
     if item_id in list(cart.keys()):
         cart[item_id] += quantity
+        messages.success(request, 'Item successfully added to cart!')
     else:
         cart[item_id] = quantity
+        messages.success(request, 'Item successfully added to cart!')
 
     request.session['cart'] = cart
     return redirect(redirect_url)
@@ -41,6 +44,7 @@ def edit_cart(request, item_id):
 
     if quantity > 0:
         cart[item_id] = quantity
+        messages.success(request, 'Item updated successfully')
 
     request.session['cart'] = cart
     return redirect('cart')
@@ -55,6 +59,7 @@ def remove_from_cart(request, item_id):
 
     if item_id in cartitems:
         cart.pop(item_id, None)
+        messages.success(request, 'Item removed from cart')
   
     request.session['cart'] = cart
     return redirect('cart')
