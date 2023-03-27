6de8207234c73a0d14aad731ce86658c070b0fdd
caoimheD <caoimhe.db5@gmail.com>
2023-03-27 09:43:43 +0000

    add stripe payment box to checkout page with documentation

--- checkout/urls.py
+++ checkout/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.urls import path
 from . import views
-from checkout.views import CreateOrder
+from checkout.views import StripeIntentView, checkout
 
 urlpatterns = [
-    path('', CreateOrder.as_view(), name='checkout'),
-    path('config/', views.stripe_config),
-    path('create-checkout-session/', views.create_checkout_session),
+    #path('', CreateOrder.as_view(), name='checkout'),
+    path('', views.checkout, name='checkout'),
+    #path('create-checkout-session/', views.create_checkout_session),
+    path('create-payment-intent/', StripeIntentView.as_view(), name='create-payment-intent'),
 ]
--- checkout/views.py
+++ checkout/views.py
@@ -2,14 +2,17 @@ from django.shortcuts import render
 from .models import Order, OrderLineItem
 from django.views.generic import ListView, DetailView, UpdateView, \
     CreateView, DeleteView
+from django.views import View
 
 import stripe
+import json
 
 from django.conf import settings 
 from django.http.response import JsonResponse 
 from django.views.decorators.csrf import csrf_exempt 
 
-# Create your views here.
+stripe_public_key = settings.STRIPE_PUBLIC_KEY
+stripe_secret_key = settings.STRIPE_SECRET_KEY
 
 
 #class Order(ListView):
@@ -22,19 +25,22 @@ from django.views.decorators.csrf import csrf_exempt
 #        messages.error(request, "There's nothing in your bag at the moment")
 
 
-class CreateOrder(CreateView):
-    model = Order
-    fields = 'full_name', 'email', 'phone_number', 'street_address1', \
-             'street_address2', 'town_or_city', 'postcode', 'county', \
-             'country'
-    template_name = '../templates/checkout/checkout.html'
-    context_object_name = 'checkout'
+#class CreateOrder(CreateView):
+#    model = Order
+#    fields = 'full_name', 'email', 'phone_number', 'street_address1', \
+#             'street_address2', 'town_or_city', 'postcode', 'county', \
+#             'country'
+#    template_name = '../templates/checkout/checkout.html'
+#    context_object_name = 'checkout'
 
-    def cart(self, request):
-        cart = request.session.get('cart', {})
-        return cart
+#    def cart(self, request):
+#        cart = request.session.get('cart', {})
+#        return cart
 
-"""
+#    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+#    stripe_secret_key = settings.STRIPE_SECRET_KEY
+
+
 def checkout(request):
     stripe_public_key = settings.STRIPE_PUBLIC_KEY
     stripe_secret_key = settings.STRIPE_SECRET_KEY
@@ -44,28 +50,35 @@ def checkout(request):
         messages.error(request, "There's nothing in your bag at the moment")
         return redirect(reverse('products'))
 
-    current_cart = cart_contents(request)
-    total = current_bag['grand_total']
-    stripe_total = round(total * 100)
-    stripe.api_key = stripe_secret_key
-    intent = stripe.PaymentIntent.create(
-        amount=stripe_total,
-        currency=settings.STRIPE_CURRENCY,
-    )
-
-    order_form = CreateOrder()
-
-    if not stripe_public_key:
-        messages.warning(request, 'Stripe public key is missing. \
-            Did you forget to set it in your environment?')
-
+   # order_form = OrderForm()
     template = 'checkout/checkout.html'
     context = {
-        'order_form': order_form,
+        #'order_form': order_form,
         'stripe_public_key': stripe_public_key,
-        'client_secret': intent.client_secret,
+        'client_secret': stripe_secret_key,
     }
 
     return render(request, template, context)
 
-"""
+
+
+# From Stripe documentation
+
+
+class StripeIntentView(View):
+    def post(self, request, *args, **kwargs):
+        try:
+            data = json.loads(request.data)
+        # Create a PaymentIntent with the order amount and currency
+            intent = stripe.PaymentIntent.create(
+                amount=calculate_order_amount(data['items']),
+                currency='eur',
+                automatic_payment_methods={
+                 'enabled': True,
+                },
+            )
+            return jsonify({
+                'clientSecret': intent['client_secret']
+            })
+        except Exception as e:
+            return jsonify(error=str(e)), 403
