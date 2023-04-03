dbf7abc250ab0b48850da37492daf1ec7c7ea011
caoimheD <caoimhe.db5@gmail.com>
2023-04-03 13:16:24 +0000

    add crud functionality for products (templates, views and urls)

--- products/urls.py
+++ products/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.urls import path
 from . import views
-from products.views import ProductList, ProductDetail, CategoryList, CategoryDetails
+from products.views import ProductList, ProductDetail, CategoryList, \
+    CategoryDetails, ProductCreate, ProductUpdate, ProductDelete
 
 
 urlpatterns = [
@@ -9,4 +10,9 @@ urlpatterns = [
     path('<int:pk>/', ProductDetail.as_view(), name='productdetail'),
     path('', CategoryList.as_view(), name='categories'),
     path('categories/<int:pk>/', CategoryDetails.as_view(), name='categorydetails'),
+    path('new/', ProductCreate.as_view(), name='createproduct'),
+    path('<int:pk>/delete/', ProductDelete.as_view(),
+         name='deleteproduct'),
+    path('<int:pk>/update/', ProductUpdate.as_view(),
+         name='updateproduct'),
 ]
--- products/views.py
+++ products/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
+from django.urls import reverse, reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib import messages
 from .models import Product, Category
 from django.views.generic import ListView, DetailView, UpdateView, \
     CreateView, DeleteView
 
-# Create your views here.
+# To view products and product details
 
 
 class ProductList(ListView):
@@ -17,6 +20,8 @@ class ProductDetail(DetailView):
     template_name = '../templates/products/product_detail.html'
     context_object_name = 'productdetails'
 
+# To view categories
+
 
 class CategoryList(ListView):
     model = Category
@@ -28,3 +33,42 @@ class CategoryDetails(DetailView):
     model = Category
     template_name = '../templates/products/product_category.html'
     context_object_name = 'categorydetails'
+
+# Admin CRUD functionality
+
+
+class ProductCreate(CreateView):
+    model = Product
+    fields = '__all__'
+    template_name = '../templates/products/create_product.html'
+    context_object_name = 'createproducts'
+
+    def get_success_url(self):
+        return reverse_lazy('products')
+
+
+class ProductUpdate(UpdateView):
+    model = Product
+    fields = '__all__'
+    template_name = '../templates/products/update_product.html'
+    context_object_name = 'updateproducts'
+
+    def get_success_url(self):
+        return reverse_lazy('productdetail', kwargs={'pk': self.object.pk})
+
+    def form_valid(self, form):
+        messages.success(self.request, 'Product updated successfully!')
+        return super(ProductUpdate, self).form_valid(form)
+
+
+class ProductDelete(DeleteView):
+    model = Product
+    template_name = '../templates/products/delete_product.html'
+    context_object_name = 'deleteproducts'
+
+    def get_success_url(self):
+        return reverse_lazy('products')
+
+    def form_valid(self, form):
+        messages.success(self.request, 'Product deleted successfully!')
+        return super(ProductDelete(), self).form_valid(form)
