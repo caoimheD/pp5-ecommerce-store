35c68ca43948c736f96ba155caa311b8cbe00758
caoimheD <caoimhe.db5@gmail.com>
2023-04-07 19:40:44 +0000

    move media folder to main dir, delete empty static folder in profile app

--- ecommercestore/urls.py
+++ ecommercestore/urls.py
@@ -27,5 +27,5 @@ urlpatterns = [
     path('cart/', include('cart.urls')),
     path('checkout/', include('checkout.urls')),
     path('profile/', include('profiles.urls')),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 handler404 = 'ecommercestore.views.handler404'
